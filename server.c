9787789c0d47ab5714ec9b5a79a2ca99b5965176
Mateusz Ratkowski <88677167+mateusz-ratkowski@users.noreply.github.com>
2024-12-18 18:14:42 +0100

    Update server.c
    english translation of comments in server.c

--- server.c
+++ server.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 
 
-//zmienne globalne
+//global variables
 struct post {
     char name[64];
     char content[1024];
@@ -30,7 +30,7 @@ int request_fd;
 const char *request_fifo = "/tmp/request_fifo";
 const char *response_fifo = "/tmp/response_fifo";
 
-//handler dla Ctrl^C niszczący obiekty IPC
+//handler Ctrl^C
 void sigint_handler() {
     printf("\nSprzątanie...\n");
     if (shmdt(shared_data) == 0)
@@ -54,7 +54,7 @@ void sigint_handler() {
     exit(0);
 }
 
-//handler dla Ctr^Z wypisujący treść pamięci
+//handler Ctr^Z
 void sigtstp_handler() {
     printf("\n____________Twitter 2.0____________\n");
     for (int i = 0; i < server_capacity; i++) {
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
     signal(SIGINT, sigint_handler);
     signal(SIGTSTP, sigtstp_handler);
 
-    //generowanie różnych kluczy dla pamięci i semaforów
+    //generationg keys for shared memory and semaphores
     IPC_key_mem = ftok(file_name, 1);
     IPC_key_sem = ftok(file_name, 2);
     if (IPC_key_mem == -1 || IPC_key_sem == -1) {
@@ -84,14 +84,14 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    //tworzenie id pamięci współdzielonej
+    //creating id of shared memory
     shmid = shmget(IPC_key_mem, sizeof(struct post) * server_capacity, 0600 | IPC_CREAT | IPC_EXCL);
     if (shmid == -1) {
         printf("Błąd shmget\n");
         exit(1);
     }
 
-    //tworzenie pamięci współdzielonej
+    //allocation of shared memory
     shared_data = shmat(shmid, NULL, 0);
     if (shared_data == (void *)-1) {
         printf("Błąd shmat\n");
@@ -99,22 +99,20 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    //inicjalizacja clen i like'ów
+    //likes and clen initialisation
     for (int i = 0; i < server_capacity; i++) {
         shared_data[i].clen = 0;
         shared_data[i].likes = 0;
     }
 
 
-    //tworzenie semaforów
+    //creationg semaphores
     semid = semget(IPC_key_sem, server_capacity, 0600 | IPC_CREAT);
     if (semid == -1) {
         printf("Błąd semget\n");
         sigint_handler();
         exit(1);
     }
-
-
     for (int i = 0; i < server_capacity; i++) {
         if (semctl(semid, i, SETVAL, 1) == -1) {
             printf("Błąd semctl\n");
@@ -126,7 +124,7 @@ int main(int argc, char *argv[]) {
 
     printf("Serwer nasluchuje...\n");
 
-    //tworzenie FIFO
+    //creationg FIFO
     if (mkfifo(request_fifo, 0666) == -1 || mkfifo(response_fifo, 0666) == -1) {
         printf("Błąd mkfifo\n");
         sigint_handler();
@@ -141,7 +139,7 @@ int main(int argc, char *argv[]) {
     }
 
 
-    //pętla wychwytująca klientów
+    //client catching loop
     char request_buffer[1];
     while (1) {
         if (read(request_fd, request_buffer, sizeof(request_buffer)) > 0) {
